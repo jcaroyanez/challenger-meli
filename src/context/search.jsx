f2700b336b14214575f2e995de5c88721f9a0cd3
jose luis caro yanez <jcaroyanez@gmail.com>
2022-10-16 17:51:04 -0500

    refactor: simplify in context get params search fot initvalue input

--- src/context/search.jsx
+++ src/context/search.jsx
@@ -1,30 +1,18 @@
-import { createContext, useContext, useEffect, useState } from 'react'
-import { useSearchParams, useLocation } from 'react-router-dom'
+import { createContext, useContext } from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 export const SearchCreateContext = createContext()
-
-export const useGetTextSeach = () => {
-  const [searchParams] = useSearchParams()
-  const inputValue = searchParams.get('search') || ''
-  const [textSearch, setTextSearch] = useState(inputValue)
-  const location = useLocation()
-
-  useEffect(() => {
-    setTextSearch(searchParams.get('search') || '')
-  }, [location])
-
-  return textSearch
-}
 
 export const useSeachContext = () => {
   return useContext(SearchCreateContext)
 }
 
 export const SearchContext = ({ children }) => {
-    const textSearch = useGetTextSeach()
+    const [searchParams] = useSearchParams()
+    const inputValue = searchParams.get('search') || ''
 
     return (
-      <SearchCreateContext.Provider value={{ textSearch }}>
+      <SearchCreateContext.Provider value={{ textSearch: inputValue }}>
         {children}
       </SearchCreateContext.Provider>
     )
