eba8db3e40643ed3f6a9598783654f3e504bce70
jose luis caro yanez <jcaroyanez@gmail.com>
2022-10-17 14:09:57 -0500

    refactor: leave a single base url

--- src/definition/constant.js
+++ src/definition/constant.js
@@ -1,5 +1,4 @@
-export const URL_ITEMS = 'http://localhost:8080/api/items?q='
-export const URL_ITEM = 'http://localhost:8080/api/items/'
+export const URL_ITEMS = 'http://localhost:8080/api/items'
 
 export const MAPPER_CONDITION = {
     new: 'Nuevo',
--- src/hooks/items.js
+++ src/hooks/items.js
@@ -1,15 +1,15 @@
 import useSWR from 'swr'
-import { URL_ITEM, URL_ITEMS } from '../definition/constant'
+import { URL_ITEMS } from '../definition/constant'
 import { itemsFetch, itemFetch } from '../services/items'
 
 export function useItems (textSearch) {
-    const url = `${URL_ITEMS}${textSearch}`
+    const url = `${URL_ITEMS}?q=${textSearch}`
     const { data, error } = useSWR([url], itemsFetch, { revalidateOnFocus: false })
     return { data, error }
 }
 
 export function useItem (itemId) {
-    const url = `${URL_ITEM}${itemId}`
+    const url = `${URL_ITEMS}/${itemId}`
     const { data, error } = useSWR([url], itemFetch, { revalidateOnFocus: false })
     return { data, error }
 }
